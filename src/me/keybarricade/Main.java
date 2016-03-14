44419f7d92d6b12d48fe99717f4058d71c65b33d
Tim Vis'ee <timvisee@gmail.com>
2016-03-14 14:51:44 +0100

    Added application constants

--- src/me/keybarricade/Main.java
+++ src/me/keybarricade/Main.java
@@ -2,7 +2,28 @@ package me.keybarricade;
 
 public class Main {
 
-    public static void main(String[] args) {
+    /**
+     * Application name.
+     */
+    public static String APP_NAME = "Key Barricade";
 
+    /**
+     * Application version name.
+     */
+    public static String APP_VERSION_NAME = "0.1";
+
+    /**
+     * Application version code.
+     * This integer should be increased by one each version.
+     */
+    public static int APP_VERSION_CODE = 1;
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start up arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
