cac1abfef121a7605972e18411f3f53fa33f8285
Tim Visée <timvisee@gmail.com>
2016-03-16 22:26:46 +0100

    Create CubeRenderer and GridRenderer example components

---
+++ src/main/java/me/keybarricade/voxeltex/component/drawable/CubeRendererComponent.java
@@ -0,0 +1,65 @@
+package me.keybarricade.voxeltex.component.drawable;
+
+import org.joml.Quaternionf;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class CubeRendererComponent extends DrawableComponent {
+
+    @Override
+    public void update() { }
+
+    @Override
+    public void draw() {
+        // Call the super
+        super.draw();
+
+        // Enter quad drawing mode
+        glBegin(GL_QUADS);
+
+        // Set the color and draw the quad
+        glColor3f(0.0f, 0.0f, 0.2f );
+        glVertex3f( 0.5f, -0.5f, -0.5f );
+        glVertex3f( 0.5f,  0.5f, -0.5f );
+        glVertex3f(-0.5f,  0.5f, -0.5f );
+        glVertex3f(-0.5f, -0.5f, -0.5f );
+
+        // Set the color and draw the quad
+        glColor3f(0.0f, 0.0f, 1.0f );
+        glVertex3f( 0.5f, -0.5f,  0.5f );
+        glVertex3f( 0.5f,  0.5f,  0.5f );
+        glVertex3f(-0.5f,  0.5f,  0.5f );
+        glVertex3f(-0.5f, -0.5f,  0.5f );
+
+        // Set the color and draw the quad
+        glColor3f(1.0f, 0.0f, 0.0f );
+        glVertex3f( 0.5f, -0.5f, -0.5f );
+        glVertex3f( 0.5f,  0.5f, -0.5f );
+        glVertex3f( 0.5f,  0.5f,  0.5f );
+        glVertex3f( 0.5f, -0.5f,  0.5f );
+
+        // Set the color and draw the quad
+        glColor3f(0.2f, 0.0f, 0.0f );
+        glVertex3f(-0.5f, -0.5f,  0.5f );
+        glVertex3f(-0.5f,  0.5f,  0.5f );
+        glVertex3f(-0.5f,  0.5f, -0.5f );
+        glVertex3f(-0.5f, -0.5f, -0.5f );
+
+        // Set the color and draw the quad
+        glColor3f(0.0f, 1.0f, 0.0f );
+        glVertex3f( 0.5f,  0.5f,  0.5f );
+        glVertex3f( 0.5f,  0.5f, -0.5f );
+        glVertex3f(-0.5f,  0.5f, -0.5f );
+        glVertex3f(-0.5f,  0.5f,  0.5f );
+
+        // Set the color and draw the quad
+        glColor3f(0.0f, 0.2f, 0.0f );
+        glVertex3f( 0.5f, -0.5f, -0.5f );
+        glVertex3f( 0.5f, -0.5f,  0.5f );
+        glVertex3f(-0.5f, -0.5f,  0.5f );
+        glVertex3f(-0.5f, -0.5f, -0.5f );
+
+        // Finish drawing
+        glEnd();
+    }
+}
---
+++ src/main/java/me/keybarricade/voxeltex/component/drawable/GridRendererComponent.java
@@ -0,0 +1,34 @@
+package me.keybarricade.voxeltex.component.drawable;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glVertex3f;
+
+public class GridRendererComponent extends DrawableComponent {
+
+    @Override
+    public void update() { }
+
+    @Override
+    public void draw() {
+        // Call the super
+        super.draw();
+
+        // Enable line drawing mode
+        glBegin(GL_LINES);
+
+        // Set the grid color
+        glColor3f(0.2f, 0.2f, 0.2f);
+
+        // Draw the grid
+        for(int i = -20; i <= 20; i++) {
+            glVertex3f(-20.0f, 0.0f, i);
+            glVertex3f(20.0f, 0.0f, i);
+            glVertex3f(i, 0.0f, -20.0f);
+            glVertex3f(i, 0.0f, 20.0f);
+        }
+
+        // Finish drawing
+        glEnd();
+    }
+}
