dad5636631d3ec18ef1df4b467a608d21af87c19
Tim Visée <timvisee@gmail.com>
2016-03-16 18:52:26 +0100

    Create basic Scene classes

---
+++ src/main/java/me/keybarricade/voxeltex/scene/Scene.java
@@ -0,0 +1,74 @@
+package me.keybarricade.voxeltex.scene;
+
+import me.keybarricade.voxeltex.gameobject.AbstractGameObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scene extends AbstractScene {
+
+    /**
+     * List of game objects in this scene.
+     */
+    private List<AbstractGameObject> gameObjects = new ArrayList<>();
+
+    @Override
+    public List<AbstractGameObject> getGameObjects() {
+        return this.gameObjects;
+    }
+
+    @Override
+    public int getGameObjectCount(boolean recursive) {
+        // Return the number if not recursive
+        if(!recursive)
+            this.gameObjects.size();
+
+        // Count the number of game objects
+        int count = 0;
+
+        // Loop through all game objects and count the total
+        for(AbstractGameObject gameObject : this.gameObjects)
+            count += gameObject.getChildCount(true);
+
+        // Return the object count
+        return count;
+    }
+
+    @Override
+    public int getTotalGameObjectCount() {
+        return getGameObjectCount(true);
+    }
+
+    @Override
+    public void addGameObject(AbstractGameObject gameObject) {
+        this.gameObjects.add(gameObject);
+    }
+
+    @Override
+    public AbstractGameObject getGameObject(int i) {
+        return this.gameObjects.get(i);
+    }
+
+    @Override
+    public boolean removeGameObject(AbstractGameObject gameObject) {
+        // Remove any game object
+        if(!this.gameObjects.remove(gameObject))
+            return false;
+
+        // Return the result
+        return true;
+    }
+
+    @Override
+    public AbstractGameObject removeGameObject(int i) {
+        // Get the game object that will be removed
+        AbstractGameObject gameObject;
+
+        // Remove the game object by it's index, and make sure any child was removed
+        if((gameObject = this.gameObjects.remove(i)) == null)
+            return null;
+
+        // Return the game object
+        return gameObject;
+    }
+}
