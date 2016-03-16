3c91ce4f28206b012d498a6646af318b437c7728
Tim Visée <timvisee@gmail.com>
2016-03-16 23:20:43 +0100

    Implement new transform object in camera component

--- src/main/java/me/keybarricade/voxeltex/component/camera/CameraComponent.java
+++ src/main/java/me/keybarricade/voxeltex/component/camera/CameraComponent.java
@@ -1,6 +1,5 @@
 package me.keybarricade.voxeltex.component.camera;
 
-import me.keybarricade.voxeltex.gameobject.KinematicGameObject;
 import me.keybarricade.voxeltex.renderer.VoxelTexRenderer;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
@@ -26,13 +25,10 @@ public class CameraComponent extends AbstractCameraComponent {
 
     @Override
     public void updateCamera() {
-        // Get the camera owner
-        KinematicGameObject owner = (KinematicGameObject) getOwner();
-
         Vector3f tmp = new Vector3f();
 
         // Reset the camera acceleration
-        owner.getLinearAcceleration().zero();
+        getTransform().getLinearAcceleration().zero();
 
         // Define the acceleration factor
         float accFactor = 6.0f;
@@ -40,33 +36,33 @@ public class CameraComponent extends AbstractCameraComponent {
 
         // Handle camera inputs
         if(VoxelTexRenderer.keyDown[GLFW_KEY_W])
-            owner.getLinearAcceleration().fma(accFactor, forward(tmp));
+            getTransform().getLinearAcceleration().fma(accFactor, forward(tmp));
         if(VoxelTexRenderer.keyDown[GLFW_KEY_S])
-            owner.getLinearAcceleration().fma(-accFactor, forward(tmp));
+            getTransform().getLinearAcceleration().fma(-accFactor, forward(tmp));
         if(VoxelTexRenderer.keyDown[GLFW_KEY_D])
-            owner.getLinearAcceleration().fma(accFactor, right(tmp));
+            getTransform().getLinearAcceleration().fma(accFactor, right(tmp));
         if(VoxelTexRenderer.keyDown[GLFW_KEY_A])
-            owner.getLinearAcceleration().fma(-accFactor, right(tmp));
+            getTransform().getLinearAcceleration().fma(-accFactor, right(tmp));
         if(VoxelTexRenderer.keyDown[GLFW_KEY_Q])
             rotateZ -= 1.0f;
         if(VoxelTexRenderer.keyDown[GLFW_KEY_E])
             rotateZ += 1.0f;
         if(VoxelTexRenderer.keyDown[GLFW_KEY_SPACE])
-            owner.getLinearAcceleration().fma(accFactor, up(tmp));
+            getTransform().getLinearAcceleration().fma(accFactor, up(tmp));
         if(VoxelTexRenderer.keyDown[GLFW_KEY_LEFT_CONTROL])
-            owner.getLinearAcceleration().fma(-accFactor, up(tmp));
+            getTransform().getLinearAcceleration().fma(-accFactor, up(tmp));
 
         // Set the angular velocity of the camera
-        owner.getAngularVelocity().set(VoxelTexRenderer.mouseY, VoxelTexRenderer.mouseX, rotateZ);
+        getTransform().getAngularVelocity().set(VoxelTexRenderer.mouseY, VoxelTexRenderer.mouseX, rotateZ);
     }
 
     @Override
     public Matrix4f apply(Matrix4f m) {
         // Get the camera position
-        Vector3f pos = getOwner().getPosition();
+        Vector3f pos = getTransform().getPosition();
 
         // Apply the camera transformation to the matrix
-        return m.rotate(getOwner().getRotation()).translate(-pos.x, -pos.y, -pos.z);
+        return m.rotate(getTransform().getRotation()).translate(-pos.x, -pos.y, -pos.z);
     }
 
     @Override
