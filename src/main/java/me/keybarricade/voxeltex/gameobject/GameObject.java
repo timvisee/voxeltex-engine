4fe59787da23b8d7fa0b931f5aa112877372e299
Tim Visée <timvisee@gmail.com>
2016-03-16 15:51:52 +0100

    Add game object children and their management methods

--- src/main/java/me/keybarricade/voxeltex/gameobject/GameObject.java
+++ src/main/java/me/keybarricade/voxeltex/gameobject/GameObject.java
@@ -3,9 +3,15 @@ package me.keybarricade.voxeltex.gameobject;
 import org.joml.Quaternionf;
 import org.joml.Vector3f;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class GameObject implements GameObjectInterface {
 
-    // TODO: Add game object children.
+    /**
+     * The children of this game object.
+     */
+    private List<GameObjectInterface> children = new ArrayList<>();
 
     /**
      * Camera position.
@@ -31,6 +37,84 @@ public abstract class GameObject implements GameObjectInterface {
     public GameObject(Vector3f position, Quaternionf rotation) {
         this.position = position;
         this.rotation = rotation;
+    }
+
+    /**
+     * Get all children.
+     *
+     * @return Children.
+     */
+    public List<GameObjectInterface> getChildren() {
+        return this.children;
+    }
+
+    /**
+     * Get the number of children inside this game object.
+     *
+     * @param recursive True to also count the children of this game object's children (recursively), false to just
+     *                  count the children of this object.
+     *
+     * @return Number of children.
+     */
+    public int getChildCount(boolean recursive) {
+        // Count the number of children if not recursive
+        if(!recursive)
+            return this.children.size();
+
+        // Count the number of recursive children
+        int count = 0;
+
+        // Loop through all the children, and count
+        for(GameObjectInterface gameObject : this.children)
+            count += gameObject.getChildCount(true);
+
+        // Return the number of recursive children
+        return count;
+    }
+
+    /**
+     * Add a child to this game object.
+     *
+     * @param gameObject Game object to add.
+     */
+    public void addChild(GameObjectInterface gameObject) {
+        this.children.add(gameObject);
+    }
+
+    /**
+     * Get a child of this game object by it's index.
+     *
+     * @param i Child index.
+     *
+     * @return The child game object.
+     */
+    public GameObjectInterface getChild(int i) {
+        // TODO: Make sure we're in bound?
+
+        // Get the child by it's index
+        return this.children.get(i);
+    }
+
+    /**
+     * Remove a child from this game object.
+     *
+     * @param gameObject Game object to remove.
+     *
+     * @return True if any game object was removed, false if not.
+     */
+    public boolean removeChild(GameObjectInterface gameObject) {
+        return this.children.remove(gameObject);
+    }
+
+    /**
+     * Remove a child from this game object.
+     *
+     * @param i Index of the child to remove.
+     *
+     * @return True if any game object was removed, false if not.
+     */
+    public boolean removeChild(int i) {
+        return this.children.remove(i) != null;
     }
 
     /**
--- src/main/java/me/keybarricade/voxeltex/gameobject/GameObjectInterface.java
+++ src/main/java/me/keybarricade/voxeltex/gameobject/GameObjectInterface.java
@@ -1,6 +1,59 @@
 package me.keybarricade.voxeltex.gameobject;
 
+import java.util.List;
+
 public interface GameObjectInterface {
+
+    /**
+     * Get all children.
+     *
+     * @return Game object children.
+     */
+    List<GameObjectInterface> getChildren();
+
+    /**
+     * Get the number of children inside this game object.
+     *
+     * @param recursive True to also count the children of this game object's children (recursively), false to just
+     *                  count the children of this object.
+     *
+     * @return Number of children.
+     */
+    int getChildCount(boolean recursive);
+
+    /**
+     * Add a child to this game object.
+     *
+     * @param gameObject Game object to add.
+     */
+    void addChild(GameObjectInterface gameObject);
+
+    /**
+     * Get a child of this game object by it's index.
+     *
+     * @param i Child index.
+     *
+     * @return The child game object.
+     */
+    GameObjectInterface getChild(int i);
+
+    /**
+     * Remove a child from this game object.
+     *
+     * @param gameObject Game object to remove.
+     *
+     * @return True if any game object was removed, false if not.
+     */
+    boolean removeChild(GameObjectInterface gameObject);
+
+    /**
+     * Remove a child from this game object.
+     *
+     * @param i Index of the child to remove.
+     *
+     * @return True if any game object was removed, false if not.
+     */
+    boolean removeChild(int i);
 
     /**
      * Update the game object.
