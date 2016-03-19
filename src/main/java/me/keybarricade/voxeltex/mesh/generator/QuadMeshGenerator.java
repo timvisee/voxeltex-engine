a2be2004281f52c6367c7854ccc5eb8c5f8db3d5
Tim Visée <timvisee@gmail.com>
2016-03-19 20:13:33 +0100

    Made quad generator orientation configurable, delete unused Vector3 utilities class

--- src/main/java/me/keybarricade/voxeltex/mesh/generator/QuadMeshGenerator.java
+++ src/main/java/me/keybarricade/voxeltex/mesh/generator/QuadMeshGenerator.java
@@ -11,6 +11,26 @@ public class QuadMeshGenerator extends AbstractMeshGenerator {
     // TODO: Add axis orientation option!
 
     /**
+     * Quad orientation on the X axis.
+     */
+    public static final int ORIENTATION_X = 1;
+
+    /**
+     * Quad orientation on the Y axis.
+     */
+    public static final int ORIENTATION_Y = 2;
+
+    /**
+     * Quad orientation on the Z axis.
+     */
+    public static final int ORIENTATION_Z = 3;
+
+    /**
+     * Default quad orientation.
+     */
+    private static final int DEFAULT_ORIENTATION = ORIENTATION_Y;
+
+    /**
      * The default size of the quad.
      */
     private static final float DEFAULT_SIZE = 1.0f;
@@ -28,12 +48,31 @@ public class QuadMeshGenerator extends AbstractMeshGenerator {
     }
 
     /**
+     * Generate a quad mesh with the default size.
+     *
+     * @param orientation Quad orientation.
+     */
+    public QuadMeshGenerator(int orientation) {
+        this(orientation, DEFAULT_SIZE);
+    }
+
+    /**
      * Generate a quad mesh with the given size.
      *
      * @param size Size on both axis.
      */
     public QuadMeshGenerator(float size) {
         this(new Vector2f(size));
+    }
+
+    /**
+     * Generate a quad mesh with the given size.
+     *
+     * @param orientation Quad orientation.
+     * @param size Size on both axis.
+     */
+    public QuadMeshGenerator(int orientation, float size) {
+        this(orientation, new Vector2f(size));
     }
 
     /**
@@ -46,28 +85,84 @@ public class QuadMeshGenerator extends AbstractMeshGenerator {
     }
 
     /**
+     * Generate a quad mesh with the given size.
+     *
+     * @param orientation Quad orientation.
+     * @param size Size.
+     */
+    public QuadMeshGenerator(int orientation, Vector2f size) {
+        this(orientation, size, Vector3fFactory.zero());
+    }
+
+    /**
      * Generate a quad mesh with the given size and offset.
      *
      * @param size Size.
      * @param offset Offset.
      */
     public QuadMeshGenerator(Vector2f size, Vector3f offset) {
+        this(DEFAULT_ORIENTATION, size, offset);
+    }
+
+    /**
+     * Generate a quad mesh with the given size and offset.
+     *
+     * @param orientation Quad orientation.
+     * @param size Size.
+     * @param offset Offset.
+     */
+    public QuadMeshGenerator(int orientation, Vector2f size, Vector3f offset) {
         // Calculate half of the offset values
         Vector2f s = size.mul(0.5f, Vector2fFactory.identity());
         Vector3f o = offset.div(2.0f, Vector3fFactory.identity());
 
-        // Generate the vertexes counterclockwise
-        float[] vertexes = {
-                // Left-bottom triangle
-                o.x - s.x, o.y, o.z + s.y,
-                o.x - s.x, o.y, o.z - s.y,
-                o.x + s.x, o.y, o.z - s.y,
+        // Define the vertexes array
+        float[] vertexes = new float[0];
 
-                // Right-top triangle
-                o.x + s.x, o.y, o.z - s.y,
-                o.x + s.x, o.y, o.z + s.y,
-                o.x - s.x, o.y, o.z + s.y
-        };
+        // Generate the vertexes for the quad based on the specified orientation
+        switch(orientation) {
+            case ORIENTATION_X:
+                vertexes = new float[]{
+                        // Left-bottom triangle
+                        o.x, o.y - s.x, o.z + s.y,
+                        o.x, o.y - s.x, o.z - s.y,
+                        o.x, o.y + s.x, o.z - s.y,
+
+                        // Right-top triangle
+                        o.x, o.y + s.x, o.z - s.y,
+                        o.x, o.y + s.x, o.z + s.y,
+                        o.x, o.y - s.x, o.z + s.y
+                };
+                break;
+
+            case ORIENTATION_Y:
+                vertexes = new float[]{
+                        // Left-bottom triangle
+                        o.x - s.x, o.y, o.z + s.y,
+                        o.x - s.x, o.y, o.z - s.y,
+                        o.x + s.x, o.y, o.z - s.y,
+
+                        // Right-top triangle
+                        o.x + s.x, o.y, o.z - s.y,
+                        o.x + s.x, o.y, o.z + s.y,
+                        o.x - s.x, o.y, o.z + s.y
+                };
+                break;
+
+            case ORIENTATION_Z:
+                vertexes = new float[]{
+                        // Left-bottom triangle
+                        o.x - s.x, o.y + s.y, o.z,
+                        o.x - s.x, o.y - s.y, o.z,
+                        o.x + s.x, o.y - s.y, o.z,
+
+                        // Right-top triangle
+                        o.x + s.x, o.y - s.y, o.z,
+                        o.x + s.x, o.y + s.y, o.z,
+                        o.x - s.x, o.y + s.y, o.z
+                };
+                break;
+        }
 
         // Generate the texture coordinates in the same order
         float[] textures = {
