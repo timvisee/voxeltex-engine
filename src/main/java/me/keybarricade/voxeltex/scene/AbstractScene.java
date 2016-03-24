04ff11d3e7d2d73f4b1990f95672a0e34fe13de1
Tim Visée <timvisee@gmail.com>
2016-03-24 22:30:05 +0100

    Merged abstract scene and game object scene

--- src/main/java/me/keybarricade/voxeltex/scene/AbstractScene.java
+++ src/main/java/me/keybarricade/voxeltex/scene/AbstractScene.java
@@ -1,7 +1,11 @@
 package me.keybarricade.voxeltex.scene;
 
 import me.keybarricade.voxeltex.VoxelTexEngine;
+import me.keybarricade.voxeltex.gameobject.AbstractGameObject;
 import me.keybarricade.voxeltex.light.LightManager;
+import me.keybarricade.voxeltex.physics.ScenePhysicsEngine;
+
+import java.util.List;
 
 public abstract class AbstractScene {
 
@@ -69,6 +73,10 @@ public abstract class AbstractScene {
         if(isStarted())
             return;
 
+        // Start all game objects
+        for(AbstractGameObject gameObject : this.getGameObjects())
+            gameObject.start();
+
         // Set the started flag
         this.started = true;
     }
@@ -92,4 +100,81 @@ public abstract class AbstractScene {
      * This is called once each frame after updating the scene.
      */
     public abstract void draw();
+
+    /**
+     * Get all game objects in this scene.
+     *
+     * @return Game objects.
+     */
+    public abstract List<AbstractGameObject> getGameObjects();
+
+    /**
+     * Get the number of game objects in this scene.
+     *
+     * @param recursive True to count recursively, false to only count the root objects.
+     *
+     * @return Game object count.
+     */
+    public abstract int getGameObjectCount(boolean recursive);
+
+    /**
+     * Check whether the scene has any game objects.
+     *
+     * @return True if the scene has any game object, false if not.
+     */
+    public abstract boolean hasGameObjects();
+
+    /**
+     * Get the total number of game objects inside this scene.
+     *
+     * @return Total game object count.
+     */
+    public abstract int getTotalGameObjectCount();
+
+    /**
+     * Add a game object to the scene.
+     *
+     * @param gameObject Game object.
+     */
+    public abstract void addGameObject(AbstractGameObject gameObject);
+
+    /**
+     * Get the game object at the given index.
+     *
+     * @param i Game object index.
+     *
+     * @return Game object.
+     */
+    public abstract AbstractGameObject getGameObject(int i);
+
+    /**
+     * Remove the given game object from the scene.
+     *
+     * @return True if any game object was removed, false if not.
+     */
+    public abstract boolean removeGameObject(AbstractGameObject gameObject);
+
+    /**
+     * Remove the game object at the given index.
+     *
+     * @param i Game object index.
+     *
+     * @return The game object that was removed, or null if none was removed.
+     */
+    public abstract AbstractGameObject removeGameObject(int i);
+
+
+
+
+
+
+
+
+
+
+    private ScenePhysicsEngine physics = new ScenePhysicsEngine(this);
+
+    public ScenePhysicsEngine getPhysics() {
+        return this.physics;
+    }
 }
