1a3cbfa93b86252fcb25889609f3796670cb2fbf
Tim Visée <timvisee@gmail.com>
2016-03-28 15:51:35 +0200

    Create rigidbody components (work in progress)

---
+++ src/main/java/me/keybarricade/voxeltex/physics/rigidbody/AbstractRigidbodyComponent.java
@@ -0,0 +1,7 @@
+package me.keybarricade.voxeltex.physics.rigidbody;
+
+import me.keybarricade.voxeltex.component.BaseComponent;
+
+public abstract class AbstractRigidbodyComponent extends BaseComponent implements RigidbodyComponentInterface {
+
+}
---
+++ src/main/java/me/keybarricade/voxeltex/physics/rigidbody/RigidbodyComponentInterface.java
@@ -0,0 +1,7 @@
+package me.keybarricade.voxeltex.physics.rigidbody;
+
+/**
+ * Created by Tim on 24-3-2016.
+ */
+public interface RigidbodyComponentInterface {
+}
