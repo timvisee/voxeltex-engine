a0acb5b918da0bb7d090adcd01e26c5f5b222fac
Tim Visée <timvisee@gmail.com>
2016-03-29 20:52:39 +0200

    Handle unknown key presses with an error

--- src/main/java/me/keybarricade/voxeltex/input/key/KeyInputManager.java
+++ src/main/java/me/keybarricade/voxeltex/input/key/KeyInputManager.java
@@ -50,12 +50,18 @@ public class KeyInputManager implements KeyInputInterface {
      */
     public void init() {
         // Create and set up the key callback
-        keyCallback = new GLFWKeyCallback() {
+        this.keyCallback = new GLFWKeyCallback() {
             @Override
-            public void invoke(long window, int key, int scancode, int action, int mods) {
+            public void invoke(long window, int key, int scanCode, int action, int mods) {
                 // Schedule a renderer window close when the escape key is pressed
                 if(key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE)
                     GLFW.glfwSetWindowShouldClose(window, GL11.GL_TRUE);
+
+                // Make sure the key code is known
+                if(key < 0) {
+                    System.out.println("Unable to handle pressed key, key unknown.");
+                    return;
+                }
 
                 // Set whether the key is pressed
                 keysDown[key] = action == GLFW.GLFW_PRESS || action == GLFW.GLFW_REPEAT;
@@ -71,8 +77,8 @@ public class KeyInputManager implements KeyInputInterface {
      */
     public void destroy() {
         // Release and destroy the key input callback
-        keyCallback.release();
-        keyCallback = null;
+        this.keyCallback.release();
+        this.keyCallback = null;
     }
 
     /**
@@ -81,7 +87,7 @@ public class KeyInputManager implements KeyInputInterface {
      * @return Window.
      */
     public VoxelTexWindow getWindow() {
-        return window;
+        return this.window;
     }
 
     @Override
