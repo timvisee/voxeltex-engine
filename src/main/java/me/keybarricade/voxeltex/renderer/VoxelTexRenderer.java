3cfb78db7c6a9ee73a49c843bdab2d5ac3490f97
Tim Visée <timvisee@gmail.com>
2016-04-03 00:59:07 +0200

    Implement face culling on all back faces to improve performance

--- src/main/java/me/keybarricade/voxeltex/renderer/VoxelTexRenderer.java
+++ src/main/java/me/keybarricade/voxeltex/renderer/VoxelTexRenderer.java
@@ -244,6 +244,10 @@ public class VoxelTexRenderer extends VoxelTexBaseRenderer {
             this.window.glViewportDefault();
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+            // Enable face culling for back faces
+            glEnable(GL_CULL_FACE);
+            glCullFace(GL_BACK);
+
             // Enable alpha channel usages
             glEnable(GL_BLEND);
             glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
