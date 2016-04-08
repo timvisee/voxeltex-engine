07aafc4811e84b2eac3a656a04a60c4bb2aded65
Tim Visée <timvisee@gmail.com>
2016-04-08 03:41:01 +0200

    Add dark box tint

--- src/main/java/me/keybarricade/game/prefab/BoxPrefab.java
+++ src/main/java/me/keybarricade/game/prefab/BoxPrefab.java
@@ -42,35 +42,68 @@ public class BoxPrefab extends CubePrefab {
         // Get a random value
         float r = (float) Math.random();
 
-        // 3/4th chance to use the regular box
-        if(r < 0.75f)
-            return GameResourceBundle.getInstance().MATERIAL_BOX0;
+        if(r < 0.5f) {
+            // 3/4th chance to use the regular box
+            if(r * 2.0f < 0.75f)
+                return GameResourceBundle.getInstance().MATERIAL_BOX0;
 
-        else {
-            // Re-randomize the value
-            r = (float) Math.random();
+            else {
+                // Re-randomize the value
+                r = (float) Math.random();
 
-            // Pick the correct material
-            if(r < 0.15f)
-                return GameResourceBundle.getInstance().MATERIAL_BOX1;
-            else if(r < 0.3f)
-                return GameResourceBundle.getInstance().MATERIAL_BOX2;
-            else if(r < 0.4f)
-                return GameResourceBundle.getInstance().MATERIAL_BOX3;
-            else if(r < 0.5f)
-                return GameResourceBundle.getInstance().MATERIAL_BOX4;
-            else if(r < 0.55f)
-                return GameResourceBundle.getInstance().MATERIAL_BOX5;
-            else if(r < 0.6f)
-                return GameResourceBundle.getInstance().MATERIAL_BOX6;
-            else if(r < 0.7f)
-                return GameResourceBundle.getInstance().MATERIAL_BOX7;
-            else if(r < 0.8f)
-                return GameResourceBundle.getInstance().MATERIAL_BOX8;
-            else if(r < 0.95f)
-                return GameResourceBundle.getInstance().MATERIAL_BOX9;
-            else
-                return GameResourceBundle.getInstance().MATERIAL_BOX10;
+                // Pick the correct material
+                if(r < 0.15f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX1;
+                else if(r < 0.3f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX2;
+                else if(r < 0.4f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX3;
+                else if(r < 0.5f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX4;
+                else if(r < 0.55f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX5;
+                else if(r < 0.6f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX6;
+                else if(r < 0.7f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX7;
+                else if(r < 0.8f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX8;
+                else if(r < 0.95f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX9;
+                else
+                    return GameResourceBundle.getInstance().MATERIAL_BOX10;
+            }
+        } else {
+            // 3/4th chance to use the regular box
+            if((r - 0.5f) * 2.0f < 0.75f)
+                return GameResourceBundle.getInstance().MATERIAL_BOX0_DARK;
+
+            else {
+                // Re-randomize the value
+                r = (float) Math.random();
+
+                // Pick the correct material
+                if(r < 0.15f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX1_DARK;
+                else if(r < 0.3f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX2_DARK;
+                else if(r < 0.4f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX3_DARK;
+                else if(r < 0.5f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX4_DARK;
+                else if(r < 0.55f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX5_DARK;
+                else if(r < 0.6f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX6_DARK;
+                else if(r < 0.7f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX7_DARK;
+                else if(r < 0.8f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX8_DARK;
+                else if(r < 0.95f)
+                    return GameResourceBundle.getInstance().MATERIAL_BOX9_DARK;
+                else
+                    return GameResourceBundle.getInstance().MATERIAL_BOX10_DARK;
+            }
         }
     }
 }
