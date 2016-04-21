03aa16bfaa32fb57fc460fd4cef4b26e108564ad
Tim Visée <timvisee@gmail.com>
2016-04-21 20:18:44 +0200

    Move padlock prefab logic into finish controller component

--- src/main/java/com/timvisee/keybarricade/game/prefab/PadlockPrefab.java
+++ src/main/java/com/timvisee/keybarricade/game/prefab/PadlockPrefab.java
@@ -24,7 +24,7 @@ package com.timvisee.keybarricade.game.prefab;
 
 import com.timvisee.keybarricade.game.LockType;
 import com.timvisee.keybarricade.game.asset.GameResourceBundle;
-import com.timvisee.keybarricade.game.component.animator.ObjectDecayAnimatorComponent;
+import com.timvisee.keybarricade.game.component.padlock.PadlockControllerComponent;
 import com.timvisee.keybarricade.game.component.player.PlayerControllerComponent;
 import com.timvisee.voxeltex.component.collider.primitive.BoxColliderComponent;
 import com.timvisee.voxeltex.component.light.LightSourceComponent;
@@ -46,29 +46,9 @@ public class PadlockPrefab extends GameObject {
     private static final String GAME_OBJECT_NAME = "PadlockPickupPrefab";
 
     /**
-     * Distance trigger.
+     * Padlock controller component instance.
      */
-    private static final float PICKUP_TRIGGER_DISTANCE = 1.1f;
-
-    /**
-     * Reference to the player controller component. Used to calculate whether to pickup the key or not.
-     */
-    private PlayerControllerComponent playerController;
-
-    /**
-     * Force field.
-     */
-    private CubePrefab forceField;
-
-    /**
-     * Key for the given lock lockType.
-     */
-    private LockType lockType;
-
-    /**
-     * True if this lock has been unlocked.
-     */
-    private boolean unlocked = false;
+    private final PadlockControllerComponent padlockController;
 
     /**
      * Constructor.
@@ -102,9 +82,8 @@ public class PadlockPrefab extends GameObject {
         // Construct the parent with the proper size
         super(name);
 
-        // Set the player controller component and lockType
-        this.playerController = playerController;
-        this.lockType = lockType;
+        // Create and add the padlock controller component
+        addComponent(this.padlockController = new PadlockControllerComponent(playerController, lockType));
 
         // Generate the padlock material
         Material lockMaterial = new Material(Texture.fromColor(lockType.getColor(), 1, 1));
@@ -121,87 +100,19 @@ public class PadlockPrefab extends GameObject {
         addChild(padlockModelObject);
 
         // Create a child game object that holds the force field
-        this.forceField = new CubePrefab("ForceField");
+        CubePrefab forceField = new CubePrefab("ForceField");
         forceField.getTransform().getPosition().set(0, 0.5f, 0);
         forceField.setMaterial(new Material(Texture.fromColor(Color.RED, 1, 1)));
         forceField.getMeshRenderer().setAlpha(0f);
         addChild(forceField);
+
+        // Set the padlock controller component force field instance
+        this.padlockController.setForceField(forceField);
 
         // Create a child game object that holds the padlock light
         GameObject padlockLightObject = new GameObject("PadlockLight");
         padlockLightObject.getTransform().getPosition().y = 0.75f;
         padlockLightObject.addComponent(new LightSourceComponent(Light.LIGHT_TYPE_POINT, lockType.getColor().toVector3f(), 0.2f));
         addChild(padlockLightObject);
-    }
-
-    @Override
-    public synchronized void update() {
-        // Call the super
-        super.update();
-
-        // Make sure a player controller reference is given
-        if(this.playerController != null && this.playerController.getOwner() != null) {
-            // Calculate the distance (squared) to the player controller
-            float distance = this.playerController.getTransform().getPosition().distanceSquared(getTransform().getPosition());
-
-            // Determine whether to pickup the item, trigger the player controller if that's the case
-            if(distance <= PICKUP_TRIGGER_DISTANCE * PICKUP_TRIGGER_DISTANCE && !this.unlocked) {
-                // Trigger the playerController
-                this.playerController.onTrigger(this);
-
-                // Make sure the player controller has the correct key type
-                if(this.lockType.equals(this.playerController.getPickupLockType())) {
-                    // Decay the padlock
-                    addComponent(new ObjectDecayAnimatorComponent(0.0f));
-
-                    // Set the unlocked flag
-                    this.unlocked = true;
-                }
-            }
-
-            // Calculate the force field intensity
-            float forceFieldIntensity = 0;
-            if(this.playerController.getPickupLockType() != this.lockType && !this.unlocked)
-                forceFieldIntensity = Math.max(Math.min((2.5f - distance) / 5f, 0.45f), 0);
-
-            // Update the force field intensity
-            this.forceField.getMeshRenderer().setAlpha(forceFieldIntensity);
-        }
-    }
-
-    /**
-     * Get the attached player controller component.
-     *
-     * @return Attached player controller component.
-     */
-    public PlayerControllerComponent getPlayerController() {
-        return this.playerController;
-    }
-
-    /**
-     * Set the attached player controller component.
-     *
-     * @param playerController Player controller component.
-     */
-    public void setPlayerController(PlayerControllerComponent playerController) {
-        this.playerController = playerController;
-    }
-
-    /**
-     * Get the lock lockType.
-     *
-     * @return Lock lockType.
-     */
-    public LockType getLockType() {
-        return this.lockType;
-    }
-
-    /**
-     * Set the lock lockType.
-     *
-     * @param lockType Lock lockType.
-     */
-    public void setLockType(LockType lockType) {
-        this.lockType = lockType;
     }
 }
